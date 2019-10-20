92c1af0c3cc9d594f15c9acf22a32867a33cfa17
quxueyao <2027198200@qq.com>
2019-10-20 13:50:25 +0800

    报运平台提交

--- export_web_manager/src/main/java/cn/itcast/web/controller/cargo/ExportController.java
+++ export_web_manager/src/main/java/cn/itcast/web/controller/cargo/ExportController.java
@@ -4,15 +4,22 @@ import cn.itcast.domain.cargo.*;
 import cn.itcast.service.cargo.ContractService;
 import cn.itcast.service.cargo.ExportProductService;
 import cn.itcast.service.cargo.ExportService;
+import cn.itcast.vo.ExportProductVo;
+import cn.itcast.vo.ExportResult;
+import cn.itcast.vo.ExportVo;
 import cn.itcast.web.controller.BaseController;
 import com.alibaba.druid.util.StringUtils;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.github.pagehelper.PageInfo;
+
+import org.apache.cxf.jaxrs.client.Client;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -148,5 +155,52 @@ public class ExportController extends BaseController {
             exportService.update(export);
         }
         return "redirect:contractList.do";
+    }
+
+    /**
+     * 出口报运列表，点击取消
+     */
+    @RequestMapping("/cancel")
+    public String cancel(String id) {
+        Export export = exportService.findById(id);
+        export.setState(0);
+        exportService.update(export);
+        return "redirect:/cargo/export/list.do";
+    }
+
+    /**
+     * 出口报运列表，点击提交
+     */
+    @RequestMapping("/submit")
+    public String submit(String id) {
+        Export export = exportService.findById(id);
+        export.setState(1);
+        exportService.update(export);
+        return "redirect:/cargo/export/list.do";
+    }
+
+    @RequestMapping("/exportE")
+    public String exportE(String id){
+        Export export = exportService.findById(id);
+        ExportVo exportVo = new ExportVo();
+        BeanUtils.copyProperties(export,exportVo);
+        ExportProductExample exportProductExample = new ExportProductExample();
+        exportProductExample.createCriteria().andExportIdEqualTo(id);
+        List<ExportProduct> exportProductList = exportProductService.findAll(exportProductExample);
+        List<ExportProductVo> list = new ArrayList<>(16);
+        for (ExportProduct exportProduct : exportProductList) {
+            ExportProductVo exportProductVo =new ExportProductVo();
+            BeanUtils.copyProperties(exportProduct,exportProductVo);
+            exportProductVo.setExportProductId(exportProduct.getId());
+            list.add(exportProductVo);
+        }
+        exportVo.setProducts(list);
+        exportVo.setExportId(export.getId());
+        WebClient client = WebClient.create("http://localhost:8091/ws/export/user");
+        client.post(exportVo);
+        WebClient webClient = WebClient.create("http://localhost:8091/ws/export/user/" + id);
+        ExportResult result = webClient.get(ExportResult.class);
+        exportService.updateExport(result);
+        return "redirect:list.do";
     }
 }
